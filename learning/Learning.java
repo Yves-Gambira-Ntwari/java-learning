a83b9038bf79b2ee4fd9a39c4011a60b86dc5ae5
Gambira <gambirayves@gmail.com>
2025-11-26 19:48:57 +0200

    chore: rectangle

--- learning/Learning.java
+++ learning/Learning.java
@@ -33,4 +33,10 @@ public class Learning {
 			}
 			System.out.println();
 		}
+		for(int i = 5; i >=0; i--) {
+			for(int j = 0; j<=5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 }
