5772cc585a6771d23e07dc00d001f92b0c7d1a81
Gambira <gambirayves@gmail.com>
2025-11-26 20:06:37 +0200

    chore: Other Pyramid

--- learning/Learning.java
+++ learning/Learning.java
@@ -61,4 +61,10 @@ public class Learning {
 			 for(int j = 1; j <= i; j++)System.out.print("*");
 			 System.out.println();
 		 }
+
+		 	        for (int i = 1; i <= 5; i++) {
+	        	for(int s = 1; s <= 5 - i; s++) System.out.print(" ");
+	        	for(int j = 1; j <= 2 * i - 1; j++) System.out.print("*");
+	        	System.out.println();
+	        }
 }
