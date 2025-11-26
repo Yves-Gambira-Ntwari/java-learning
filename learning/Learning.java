1f8febbd3844191cb134fb473d50f8eca325b9f9
Gambira <gambirayves@gmail.com>
2025-11-26 20:10:37 +0200

    chore: harrowed triangle

--- learning/Learning.java
+++ learning/Learning.java
@@ -72,5 +72,12 @@ public class Learning {
 	        	for(int j = 1; j <= 2 * i -1; j++) System.out.print("*");
 	        	System.out.println();
 	        }
+						        for (int i = 1; i <= 5; i++) {
+	            for (int j = 1; j <= i; j++) {
+	                if (i == 1 || j == 1 || j == i || i == 5) System.out.print("*");
+	                else System.out.print(" ");
+	            }
+	            System.out.println();
+	        }
 	        
 }
