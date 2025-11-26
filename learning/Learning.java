5da614c0cfbace1f3894093c6dc065dd58636b62
Gambira <gambirayves@gmail.com>
2025-11-26 20:03:07 +0200

    chore: right aligned reversed  triangle

--- learning/Learning.java
+++ learning/Learning.java
@@ -56,4 +56,9 @@ public class Learning {
 			 for(int j = 1; j <= i; j++)System.out.print("*");
 			 System.out.println();
 		 }
+		 for(int i = 5; i >=1; i--) {
+			 for(int k = 1; k <= 5 - i; k++) System.out.print(" ");
+			 for(int j = 1; j <= i; j++)System.out.print("*");
+			 System.out.println();
+		 }
 }
