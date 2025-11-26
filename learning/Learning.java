1c8b2e52c3f902b236ec77803b744a607d1977fc
Gambira <gambirayves@gmail.com>
2025-11-26 20:23:03 +0200

    chore: hallowed diamond

--- learning/Learning.java
+++ learning/Learning.java
@@ -95,5 +95,21 @@ public class Learning {
 	            }
 	            System.out.println();
 	        }
+					        for(int i = 1; i <= 5; i++) {
+	        	for(int s = 1; s<= 5 - i; s++)System.out.print(" ");
+	        	for(int j = 1; j <= 2*i-1; j++) {
+	        		if( j== 1|| j== 2*i-1)System.out.print("*");
+	        		else System.out.print(" ");
+	        	}
+	        	System.out.println();
+	        }
+	        for(int i = 5; i >= 1; i--) {
+	        	for(int s = 1; s<= 5 - i; s++)System.out.print(" ");
+	        	for(int j = 1; j <= 2*i-1; j++) {
+	        		if( j==1|| j== 2*i-1)System.out.print("*");
+	        		else System.out.print(" ");
+	        	}
+	        	System.out.println();
+	        }
 	        
 }
