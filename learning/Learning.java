bd12c08165198d8ab321f8032c8cea40775ecb75
Gambira <gambirayves@gmail.com>
2025-11-26 19:57:48 +0200

    chore: Pyramid

--- learning/Learning.java
+++ learning/Learning.java
@@ -39,4 +39,15 @@ public class Learning {
 			}
 			System.out.println();
 		}
+			        for(int i = 1; i <=10; i++) {
+	        	for(int j = 1; j<=i; j++) {
+	        		if( j< 10 - i) {
+	        			System.out.print(" ");
+	        		} else {
+	        			System.out.print("*");
+	        			
+	        		}
+	        	}
+	        	System.out.println();
+	        }
 }
