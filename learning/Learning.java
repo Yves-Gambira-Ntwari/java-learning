7425b14aa8c4a209a5af6adb0a671ca67ab3666c
Gambira <gambirayves@gmail.com>
2025-11-26 20:23:38 +0200

    chore: Diamond

--- learning/Learning.java
+++ learning/Learning.java
@@ -111,5 +111,16 @@ public class Learning {
 	        	}
 	        	System.out.println();
 	        }
+						        for(int i = 1; i <= 5; i++) {
+	        	for(int s = 1; s<= 5 - i; s++)System.out.print(" ");
+	        	for(int j = 1; j <= 2*i-1; j++)System.out.print("*");
+	        	System.out.println();
+	        }
+	        for(int i = 5; i >= 1; i--) {
+	        	for(int s = 1; s<= 5 - i; s++)System.out.print(" ");
+	        	for(int j = 1; j <= 2*i-1; j++)System.out.print("*");
+	        	System.out.println();
+	        }
+		
 	        
 }
