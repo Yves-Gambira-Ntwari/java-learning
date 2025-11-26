9aed726648cb4b0d9ca318c374409f95d183f342
Gambira <gambirayves@gmail.com>
2025-11-26 20:15:22 +0200

    chore: harrowed Pyramid

--- learning/Learning.java
+++ learning/Learning.java
@@ -79,5 +79,13 @@ public class Learning {
 	            }
 	            System.out.println();
 	        }
+						        for (int i = 1; i <= 5; i++) {
+	            for (int j = 1; j <= 5 - i; j++)System.out.print(" ");
+	            for(int k = 1; k <= 2 * i - 1; k++) {
+	            	if(i == 5 || k == 1 || k == 2*i-1) System.out.print("*");
+	            	else System.out.print(" ");
+	            }
+	            System.out.println();
+	        }
 	        
 }
