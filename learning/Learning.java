5d406236e43804c205af76f27d90def1118a13d8
Gambira <gambirayves@gmail.com>
2025-11-26 20:02:20 +0200

    chore: right aligned triangle

--- learning/Learning.java
+++ learning/Learning.java
@@ -50,4 +50,10 @@ public class Learning {
 	        	}
 	        	System.out.println();
 	        }
+
+							 for(int i = 1; i <=5; i++) {
+			 for(int k = 1; k <= 5 - i; k++) System.out.print(" ");
+			 for(int j = 1; j <= i; j++)System.out.print("*");
+			 System.out.println();
+		 }
 }
