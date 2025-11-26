386955e13b32f7211026495693de051285425d2b
Gambira <gambirayves@gmail.com>
2025-11-26 20:16:39 +0200

    chore: right aligned triangle with numbers

--- learning/Learning.java
+++ learning/Learning.java
@@ -87,5 +87,13 @@ public class Learning {
 	            }
 	            System.out.println();
 	        }
+
+						        int num = 1;
+	        for (int i = 1; i <= 5; i++) {
+	            for (int j = 1; j <= i; j++) {
+	                System.out.print(num++ + " ");
+	            }
+	            System.out.println();
+	        }
 	        
 }
