453987efbc4678e071eeffabd7692f46df608b46
Gambira <gambirayves@gmail.com>
2026-01-20 11:13:24 +0200

    chore: add feedback

---
+++ quiz/src/main/java/com/RetryServlet.java
@@ -0,0 +1,24 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RetryServlet
+ */
+@WebServlet("/RetryServlet")
+public class RetryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("<h1>Incorect answer try again<h1>");
+	}
+
+
+
+}
